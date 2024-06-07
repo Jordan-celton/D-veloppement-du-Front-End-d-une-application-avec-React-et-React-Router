2a6f17c47ebc38bb9c5a2484503bb8ace8b29d67
Dijoyce <celton.jordan@outlook.fr>
2024-06-07 21:11:31 +0200

    wip logement and slideshow

--- src/components/Logement.js
+++ src/components/Logement.js
@@ -1,29 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import logementsData from "../components/logements.json";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import Slideshow from "../components/Slideshow";
+import starsRating from "../assets/icon/Vector.png";
+import starsInactive from "../assets/icon/starInactive.png";
+import arrowIcon from "../assets/icon/arrow.png";
 
-// Composant fonctionnel Logement
 const Logement = () => {
-  // Récupération de l'ID de logement à partir des paramètres du fichier JSON
   const { id } = useParams();
-
-  // Recherche du logement correspondant à l'ID dans les données
   const logement = logementsData.find((logement) => logement.id === id);
+
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  const [isEquipmentsVisible, setIsEquipmentsVisible] = useState(false);
+
+  const handleDescriptionClick = () => {
+    setIsDescriptionVisible(!isDescriptionVisible);
+  };
+
+  const handleEquipmentsClick = () => {
+    setIsEquipmentsVisible(!isEquipmentsVisible);
+  };
+
+  const renderRatingStars = () => {
+    const rating = parseInt(logement.rating);
+    const totalStars = 5;
+    const stars = [];
+
+    for (let i = 0; i < totalStars; i++) {
+      if (i < rating) {
+        stars.push(
+          <img key={i} src={starsRating} alt="star" className="star" />
+        );
+      } else {
+        stars.push(
+          <img key={i} src={starsInactive} alt="empty-star" className="star" />
+        );
+      }
+    }
+
+    return stars;
+  };
 
   return (
     <div>
       <Navigation />
-      {/* Utilisation du composant Slideshow */}
       <Slideshow pictures={logement.pictures} />
-
       <h2>{logement.title}</h2>
       <p>{logement.location}</p>
-      <h3>Description</h3>
-      <p>{logement.description}</p>
-
+      <ul className="tag">
+        {logement.tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+        {renderRatingStars()}
+      </ul>
+      <h3 onClick={handleDescriptionClick}>
+        Description{" "}
+        <img
+          className={`arrow ${isDescriptionVisible ? "rotated" : ""}`}
+          src={arrowIcon}
+          alt="arrow"
+        />
+      </h3>
+      {isDescriptionVisible && <p>{logement.description}</p>}
+      <h3 onClick={handleEquipmentsClick}>
+        Equipements{" "}
+        <img
+          className={`arrow ${isEquipmentsVisible ? "rotated" : ""}`}
+          src={arrowIcon}
+          alt="arrow"
+        />
+      </h3>
+      {isEquipmentsVisible && (
+        <ul className="equipments">
+          {logement.equipments.map((equipment, index) => (
+            <li key={index}>{equipment}</li>
+          ))}
+        </ul>
+      )}
       <Footer />
     </div>
   );
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,46 +1,56 @@
-// components/Slideshow.js
-import React, { useState } from "react";
+// Slideshow.js
+import React, { useEffect, useRef } from "react";
 
 const Slideshow = ({ pictures }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideIndex = useRef(0);
+  const slideshowContainer = useRef(null);
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === pictures.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+  useEffect(() => {
+    const nextButton = slideshowContainer.current.querySelector(".next-slide");
+    const prevButton = slideshowContainer.current.querySelector(".prev-slide");
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
-    );
-  };
+    const showSlides = (n) => {
+      const slides = slideshowContainer.current.querySelectorAll(".slide");
+      if (n >= slides.length) slideIndex.current = 0;
+      if (n < 0) slideIndex.current = slides.length - 1;
+      slides.forEach((slide, index) => {
+        slide.style.display = index === slideIndex.current ? "block" : "none";
+      });
+    };
+
+    const nextSlide = () => {
+      slideIndex.current++;
+      showSlides(slideIndex.current);
+    };
+
+    const prevSlide = () => {
+      slideIndex.current--;
+      showSlides(slideIndex.current);
+    };
+
+    nextButton.addEventListener("click", nextSlide);
+    prevButton.addEventListener("click", prevSlide);
+
+    showSlides(slideIndex.current);
+
+    return () => {
+      nextButton.removeEventListener("click", nextSlide);
+      prevButton.removeEventListener("click", prevSlide);
+    };
+  }, []);
 
   return (
-    <section className="carousel-container">
-      <div
-        className="carousel-slides"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-      >
+    <div className="slideshow" ref={slideshowContainer}>
+      <button className="prev-slide">&#10094;</button>
+      <div className="slides">
         {pictures.map((picture, index) => (
-          <div className="carousel-slide" key={index}>
-            <img
-              src={picture}
-              alt={`Slide ${index}`}
-              style={{ width: "100%", height: "auto" }}
-            />
+          <div key={index} className="slide">
+            <img src={picture} alt={`Slide ${index}`} />
           </div>
         ))}
       </div>
-      <div className="carousel-controls">
-        <button className="carousel-control" onClick={prevSlide}>
-          &#10094;
-        </button>
-        <button className="carousel-control" onClick={nextSlide}>
-          &#10095;
-        </button>
-      </div>
-    </section>
+      <button className="next-slide">&#10095;</button>
+    </div>
   );
 };
 
